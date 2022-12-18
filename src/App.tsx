c4d7a7c86e8d950b32093331435f3a7c9cba71df
William Ramundo <wram19@gmail.com>
2022-12-17 23:07:13 -0500

    example of type request

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import SpeciesInput from "./components/SpeciesInput";
 import StarRatingInput from "./components/StarRatingInput";
 import TeraTypeInput, { TeraType } from "./components/TeraTypeInput";
 import sixStar from "./data/sixStar";
+import PokeAPI from "pokeapi-typescript";
 
 function App() {
 	const [starRating, setStarRating] = useState<number>(6);
 	const [selectedSpecies, setSelectedSpecies] = useState<string>("Tauros");
 	const [teraType, setTeraType] = useState<TeraType | undefined>();
+	const getTypes = async () => {
+		const result = await PokeAPI.Pokemon.fetch(selectedSpecies);
+		alert(JSON.stringify(result.types));
+		//generate weaknesses and strengths of terra type
+	}
 
 	return (
 		<>
@@ -21,6 +27,7 @@ function App() {
 				/>
 				<TeraTypeInput value={teraType} onChange={setTeraType} />
 			</section>
+			<button onClick={getTypes}>Click Here!</button>
 		</>
 	);
 }
